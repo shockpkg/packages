8db941914af36349904e0b99ba6eddb92709f7ee
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2018-07-10 02:10:04 -0400

    Refactored API update code

---
+++ updateapi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+curl -s -X POST \
+   -H 'Content-Type: application/json' \
+   -H 'Accept: application/json' \
+   -H 'Travis-API-Version: 3' \
+   -H "Authorization: token $TRAVIS_TOKEN" \
+   -d '{"request":{"branch":"dev"}}' \
+   'https://api.travis-ci.org/repo/flpkg%2Fflpkg.github.io/requests'
