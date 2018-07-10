4636e1c9a36d5242262db9e8d33da59fc37aef26
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2018-07-10 02:45:16 -0400

    Fixed linting

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -4,6 +4,8 @@
 
 var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
+// Only enable if requested, generates tons of output.
+// eslint-disable-next-line no-process-env
 if (/(1|true|yes)/i.test(process.env.SPEC_REPORTER)) {
 	jasmine.getEnv().clearReporters();
 	jasmine.getEnv().addReporter(new SpecReporter({
