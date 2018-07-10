75cbebf73f19beeb957caebd9305917f4fe3e155
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2018-07-10 02:28:47 -0400

    Made spec reporter optional to reduce CI logs

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -4,10 +4,12 @@
 
 var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new SpecReporter({
-	spec: {
-		displayPending: true,
-		displayStacktrace: true
-	}
-}));
+if (/(1|true|yes)/i.test(process.env.SPEC_REPORTER)) {
+	jasmine.getEnv().clearReporters();
+	jasmine.getEnv().addReporter(new SpecReporter({
+		spec: {
+			displayPending: true,
+			displayStacktrace: true
+		}
+	}));
+}
