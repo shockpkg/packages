10523697d67f2d8213cbfa45dacdd89d2e63af07
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2018-09-08 01:16:23 -0400

    Namespaced packages by product

--- test/rename.test.js
+++ test/rename.test.js
@@ -29,28 +29,28 @@ describe('rename', () => {
 	describe('childs', () => {
 		// Whitelist of renamed files.
 		const renamed = {
-			'player-9.0.246.0-mac-ub-npapi-debug':
+			'flash-player-9.0.246.0-mac-ub-npapi-debug':
 				'flashplayer9r246_ub_mac_debug.zip',
 
-			'player-10.0.42.34-mac-npapi-debug':
+			'flash-player-10.0.42.34-mac-npapi-debug':
 				'flashplayer10r42_34_ub_mac_debug.zip',
 
-			'player-11.0.1.152-linux-i386-npapi-debug':
+			'flash-player-11.0.1.152-linux-i386-npapi-debug':
 				'flashplayer11_0r1_152_linux_debug.i386.tar.gz',
 
-			'player-11.0.1.152-windows-64bit-uninstaller':
+			'flash-player-11.0.1.152-windows-64bit-uninstaller':
 				'uninstall_flashplayer11_0r1_152_win_64bit.exe',
 
-			'player-11.2.202.223-solaris-sparc-npapi':
+			'flash-player-11.2.202.223-solaris-sparc-npapi':
 				'flash_player11_2r202_223_solaris_sparc.tar.bz2',
 
-			'player-11.2.202.223-solaris-x86-npapi':
+			'flash-player-11.2.202.223-solaris-x86-npapi':
 				'flash_player_11_2r202_223_solaris_x86.tar.bz2',
 
-			'player-11.5.502.110-windows-npapi-debug':
+			'flash-player-11.5.502.110-windows-npapi-debug':
 				'flashplayer11_5r502_110_win_debug.exe',
 
-			'player-12.0.0.70-windows-uninstaller':
+			'flash-player-12.0.0.70-windows-uninstaller':
 				'uninstall_flashplayer12_0r0_70_win.exe'
 		};
 
