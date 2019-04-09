fdb42f296d6821eaab692f7955ad08c0cb97a526
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2019-04-09 15:21:56 -0400

    Shockwave Player is dead and gone, no need to check for updates

--- bin/newest.js
+++ bin/newest.js
@@ -7,61 +7,62 @@ const asyncQueue = require('async/queue');
 
 const {requestPromise} = require('../util/request');
 
+// Used to check for Shockwave Player updates, but downloads are all down.
 const resources = [
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/cabs/director/sw.cab',
-		size: 1292210,
-		lastModified: 'Tue, 26 Mar 2019 04:34:58 GMT',
-		eTag: '"13b7b2-584f7d7ef82db"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/Shockwave_Installer_Full.exe',
-		size: 15124440,
-		lastModified: 'Tue, 26 Mar 2019 04:34:55 GMT',
-		eTag: '"e6c7d8-584f7d7c2de38"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_full_installer.exe',
-		size: 13126832,
-		lastModified: 'Tue, 26 Mar 2019 04:34:56 GMT',
-		eTag: '"c84cb0-584f7d7d11014"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_full_installer.msi',
-		size: 24256512,
-		lastModified: 'Tue, 26 Mar 2019 04:35:03 GMT',
-		eTag: '"1722000-584f7d83b17b1"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/Shockwave_Installer_Slim.exe',
-		size: 6257824,
-		lastModified: 'Tue, 26 Mar 2019 04:34:53 GMT',
-		eTag: '"5f7ca0-584f7d7abb4d3"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_slim_installer.exe',
-		size: 4262176,
-		lastModified: 'Tue, 26 Mar 2019 04:34:54 GMT',
-		eTag: '"410920-584f7d7bb709f"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/macosx/latest/Shockwave_Installer_Full_64bit.dmg',
-		size: 18771823,
-		lastModified: 'Thu, 29 Sep 2016 10:27:21 GMT',
-		eTag: '"11e6f6f-53da2ec48cdf6"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/macosx/latest/Shockwave_Installer_Full.dmg',
-		size: 22513907,
-		lastModified: 'Thu, 04 Oct 2012 18:22:08 GMT',
-		eTag: '"15788f3-4cb3fd5409400"'
-	},
-	{
-		source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/macosx/latest/Shockwave_Installer_Slim.dmg',
-		size: 3888494,
-		lastModified: 'Thu, 04 Oct 2012 18:22:26 GMT',
-		eTag: '"3b556e-4cb3fd6533c80"'
-	}
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/cabs/director/sw.cab',
+	// 	size: 1292210,
+	// 	lastModified: 'Tue, 26 Mar 2019 04:34:58 GMT',
+	// 	eTag: '"13b7b2-584f7d7ef82db"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/Shockwave_Installer_Full.exe',
+	// 	size: 15124440,
+	// 	lastModified: 'Tue, 26 Mar 2019 04:34:55 GMT',
+	// 	eTag: '"e6c7d8-584f7d7c2de38"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_full_installer.exe',
+	// 	size: 13126832,
+	// 	lastModified: 'Tue, 26 Mar 2019 04:34:56 GMT',
+	// 	eTag: '"c84cb0-584f7d7d11014"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_full_installer.msi',
+	// 	size: 24256512,
+	// 	lastModified: 'Tue, 26 Mar 2019 04:35:03 GMT',
+	// 	eTag: '"1722000-584f7d83b17b1"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/Shockwave_Installer_Slim.exe',
+	// 	size: 6257824,
+	// 	lastModified: 'Tue, 26 Mar 2019 04:34:53 GMT',
+	// 	eTag: '"5f7ca0-584f7d7abb4d3"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_slim_installer.exe',
+	// 	size: 4262176,
+	// 	lastModified: 'Tue, 26 Mar 2019 04:34:54 GMT',
+	// 	eTag: '"410920-584f7d7bb709f"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/macosx/latest/Shockwave_Installer_Full_64bit.dmg',
+	// 	size: 18771823,
+	// 	lastModified: 'Thu, 29 Sep 2016 10:27:21 GMT',
+	// 	eTag: '"11e6f6f-53da2ec48cdf6"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/macosx/latest/Shockwave_Installer_Full.dmg',
+	// 	size: 22513907,
+	// 	lastModified: 'Thu, 04 Oct 2012 18:22:08 GMT',
+	// 	eTag: '"15788f3-4cb3fd5409400"'
+	// },
+	// {
+	// 	source: 'https://fpdownload.macromedia.com/get/shockwave/default/english/macosx/latest/Shockwave_Installer_Slim.dmg',
+	// 	size: 3888494,
+	// 	lastModified: 'Thu, 04 Oct 2012 18:22:26 GMT',
+	// 	eTag: '"3b556e-4cb3fd6533c80"'
+	// }
 ];
 
 const headerMappings = [
