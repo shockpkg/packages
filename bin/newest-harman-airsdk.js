464aaf2d905078051ad485d010e9299408737c9e
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2019-12-03 18:53:18 -0500

    Watch for harman airsdk

---
+++ bin/newest-harman-airsdk.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+'use strict';
+
+const {requestPromise} = require('../util/request');
+const harmanAirsdk = require('../util/harman-airsdk');
+
+const expected = new Map([
+	['air-sdk-33.0.2.330-windows', 613934804],
+	['air-sdk-33.0.2.330-windows-compiler', 650977571],
+	['air-sdk-33.0.2.330-mac', 580630420],
+	['air-sdk-33.0.2.330-mac-compiler', 617673128]
+]);
+
+async function main() {
+	const start = Date.now();
+
+	let failed = 0;
+	let passed = 0;
+	const list = await harmanAirsdk.list();
+	const cookie = harmanAirsdk.cookies(list.cookies);
+	for (const {name, source} of list.downloads) {
+		console.log(`Checking: ${name}`);
+		console.log(`URL: ${source}`);
+		const {response} = await requestPromise({
+			method: 'HEAD',
+			url: source,
+			headers: {
+				Cookie: cookie
+			}
+		});
+
+		if (response.statusCode !== 200) {
+			console.log(
+				`Error: ${name}: Status code: ${response.statusCode}`
+			);
+			console.log('');
+			failed++;
+			continue;
+		}
+
+		const size = +response.headers['content-length'];
+		console.log(`Size: ${size}`);
+		if (!expected.has(name)) {
+			console.log(
+				`Error: ${name}: Unknown name: ${name}`
+			);
+			console.log('');
+			failed++;
+			continue;
+		}
+
+		const sized = expected.get(name);
+		if (size !== sized) {
+			console.log(
+				`Error: ${name}: Unexpected size: ${size}`
+			);
+			console.log('');
+			failed++;
+			continue;
+		}
+
+		passed++;
+		console.log('');
+	}
+	failed += expected.size - passed;
+
+	const end = Date.now();
+
+	console.log(`Passed: ${passed}`);
+	console.log(`Failed: ${failed}`);
+	console.log(`Done after ${end - start}ms`);
+	console.log('');
+
+	if (failed) {
+		process.exitCode = 1;
+	}
+}
+main().catch(err => {
+	console.error(err);
+	process.exitCode = 1;
+});
---
+++ util/harman-airsdk.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const request = require('request');
+const puppeteer = require('puppeteer');
+
+const base = 'https://airsdk.harman.com/download';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function list() {
+	const browser = await puppeteer.launch({
+		// headless: false
+	});
+	const page = await browser.newPage();
+	await page.goto(base);
+	await page.waitForFunction(() => {
+		/* eslint-disable */
+		const el = document.querySelector(
+			'#downloadContent [type="checkbox"]'
+		);
+		if (el && !el.checked) {
+			el.click();
+		}
+		return !![...document.querySelectorAll('#downloadArea a')].filter(
+			a => a.href.includes('?id=')
+		).length;
+		/* eslint-enable */
+	});
+	const info = await page.evaluate(() => {
+		/* eslint-disable */
+		const title = document.querySelector(
+			'#downloadContent .contentTitle'
+		).textContent;
+		const downloads = [...document.querySelectorAll('#downloadArea a')].map(
+			a => a.href
+		);
+		return '' + JSON.stringify({
+			title,
+			downloads
+		});
+		/* eslint-enable */
+	});
+	const cookies = await page.cookies(base);
+	await page.close();
+	await browser.close();
+
+	const {title, downloads} = JSON.parse(info);
+
+	// Parse out the version.
+	const titleMatch = title.match(/version\s+([\d.]+)(\s|$)/i);
+	if (!titleMatch) {
+		throw new Error(`Failed to extract version from title: ${title}`);
+	}
+	const version = titleMatch[1];
+
+	// Parse out the downloads.
+	const downloadsClean = [];
+	for (const download of downloads) {
+		const file = download.split('?')[0].split('#')[0].split('/').pop();
+		const isMac = /macos/i.test(file);
+		const isWindows = /windows/i.test(file);
+		const isFlex = /flex/i.test(file);
+		if (!isMac && !isWindows) {
+			continue;
+		}
+		const name = ['air-sdk', version];
+		if (isMac) {
+			name.push('mac');
+		}
+		if (isWindows) {
+			name.push('windows');
+		}
+		if (!isFlex) {
+			name.push('compiler');
+		}
+		downloadsClean.push({
+			name: name.join('-'),
+			file,
+			source: download
+		});
+	}
+	downloadsClean.sort((a, b) => {
+		a = a.name.includes('compiler') ? 1 : 0;
+		b = b.name.includes('compiler') ? 1 : 0;
+		return a - b;
+	});
+	downloadsClean.sort((a, b) => {
+		a = a.name.includes('windows') ? 0 : 1;
+		b = b.name.includes('windows') ? 0 : 1;
+		return a - b;
+	});
+
+	// Parse out required cookies.
+	const cookiesClean = cookies.filter(c => c.name === 'JSESSIONID');
+
+	return {
+		version,
+		downloads: downloadsClean,
+		cookies: cookiesClean
+	};
+}
+exports.list = list;
+
+function cookies(list) {
+	return list.map(c => [
+		c.name,
+		c.value
+	].map(encodeURIComponent).join('=')).join('; ');
+}
+exports.cookies = cookies;
