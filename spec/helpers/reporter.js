0207cb9c646526fbdcf5fd82f0866dec6f4a6f78
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2020-03-28 16:13:16 -0400

    Dependency update

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -11,7 +11,7 @@ if (/(1|true|yes)/i.test(process.env.SPEC_REPORTER)) {
 	jasmine.getEnv().addReporter(new SpecReporter({
 		spec: {
 			displayPending: true,
-			displayStacktrace: true
+			displayStacktrace: 'pretty'
 		}
 	}));
 }
