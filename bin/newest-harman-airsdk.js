5e0afe25b6e72cfa3c2b675eda5f2d5ee08b8860
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2020-07-02 14:32:54 -0400

    Adding air sdk 33.1.1.176

--- bin/newest-harman-airsdk.js
+++ bin/newest-harman-airsdk.js
@@ -7,10 +7,10 @@ const {requestPromise} = require('../util/request');
 const harmanAirsdk = require('../util/harman-airsdk');
 
 const expected = new Map([
-	['air-sdk-33.1.1.98-windows', 600932040],
-	['air-sdk-33.1.1.98-windows-compiler', 637974807],
-	['air-sdk-33.1.1.98-mac', 597940503],
-	['air-sdk-33.1.1.98-mac-compiler', 635013157]
+	['air-sdk-33.1.1.176-windows', 603690890],
+	['air-sdk-33.1.1.176-windows-compiler', 640733657],
+	['air-sdk-33.1.1.176-mac', 587134419],
+	['air-sdk-33.1.1.176-mac-compiler', 624207086]
 ]);
 
 async function main() {
