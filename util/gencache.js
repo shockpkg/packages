6d3c5f5e31ecfc8f8187b0d47c2e8d81118e5d4e
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2021-04-30 21:13:52 -0400

    Improved gencache

--- util/gencache.js
+++ util/gencache.js
@@ -7,20 +7,27 @@ const fse = require('fs-extra');
 const {requestDownloadPromise} = require('./request');
 
 const gencacheDir = path.join(__dirname, '..', '_gencache');
+const tmpPre = '.tmp.';
 
-function cacheTmp(name) {
-	return path.join(gencacheDir, `${name}.tmp`);
+function urlFileName(url) {
+	return url.split(/[?#]/)[0].split('/').pop();
 }
 
-function cacheBin(name) {
-	return path.join(gencacheDir, `${name}.bin`);
+function cacheTmp(name, url) {
+	return path.join(gencacheDir, name, `${tmpPre}${urlFileName(url)}`);
+}
+
+function cacheBin(name, url) {
+	return path.join(gencacheDir, name, urlFileName(url));
 }
 
 async function download(name, url, onprogress = null, headers = {}) {
-	await fse.ensureDir(gencacheDir);
+	const fileCacheTmp = cacheTmp(name, url);
+	const fileCacheBin = cacheBin(name, url);
 
-	const fileCacheTmp = cacheTmp(name);
-	const fileCacheBin = cacheBin(name);
+	await Promise.all([...new Set([fileCacheTmp, fileCacheBin].map(
+		p => fse.ensureDir(path.dirname(p))
+	))]);
 
 	const fileCacheBinExists = await fse.pathExists(fileCacheBin);
 	if (!fileCacheBinExists) {
@@ -28,7 +35,6 @@ async function download(name, url, onprogress = null, headers = {}) {
 		let contentLength = null;
 		let recievedLength = 0;
 
-		// eslint-disable-next-line no-await-in-loop
 		await requestDownloadPromise(
 			{
 				url,
@@ -57,7 +63,6 @@ async function download(name, url, onprogress = null, headers = {}) {
 			);
 		}
 
-		// eslint-disable-next-line no-await-in-loop
 		await fse.move(fileCacheTmp, fileCacheBin);
 	}
 
@@ -68,9 +73,7 @@ async function download(name, url, onprogress = null, headers = {}) {
 }
 
 async function ensure(name, url, onprogress = null, headers = {}) {
-	await fse.ensureDir(gencacheDir);
-
-	const fileCacheBin = cacheBin(name);
+	const fileCacheBin = cacheBin(name, url);
 
 	let r = null;
 	const fileCacheBinExists = await fse.pathExists(fileCacheBin);
