3b10c5c85212ac7a9f55b59fa39a179010fd7959
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-08-13 00:30:43 -0400

    Test cleanup

--- bin/test.mjs
+++ bin/test.mjs
@@ -74,83 +74,21 @@ async function properties() {
 	};
 	const allowedPropsChild = new Set(Object.keys(validatorsChild));
 
-	for (const entry of roots) {
+	for (const pkg of roots) {
 		for (const p of Object.keys(validatorsRoot)) {
-			validatorsRoot[p](true, entry[p]);
+			validatorsRoot[p](true, pkg[p]);
 		}
-		for (const p of Object.keys(entry)) {
+		for (const p of Object.keys(pkg)) {
 			ok(allowedPropsRoot.has(p));
 		}
 	}
 
-	for (const entry of children) {
+	for (const pkg of children) {
 		for (const p of Object.keys(validatorsChild)) {
-			validatorsChild[p](false, entry[p]);
+			validatorsChild[p](false, pkg[p]);
 		}
-		for (const p of Object.keys(entry)) {
+		for (const p of Object.keys(pkg)) {
 			ok(allowedPropsChild.has(p));
-		}
-	}
-}
-
-async function rename() {
-	const {roots, children} = await readPackages();
-
-	const renamedRoot = {
-		// None currently.
-	};
-
-	for (const entry of roots) {
-		if (entry.name in renamedRoot) {
-			equal(entry.file, renamedRoot[entry.name]);
-		}
-		else {
-			const urlFile = decodeURIComponent(
-				(new URL(entry.source)).pathname
-					.split('/').pop()
-			);
-			equal(entry.file, urlFile);
-		}
-	}
-
-	const renamedChild = {
-		'flash-player-9.0.246.0-mac-ub-npapi-debug':
-			'flashplayer9r246_ub_mac_debug.zip',
-
-		'flash-player-10.0.42.34-mac-npapi-debug':
-			'flashplayer10r42_34_ub_mac_debug.zip',
-
-		'flash-player-11.0.1.152-linux-i386-npapi-debug':
-			'flashplayer11_0r1_152_linux_debug.i386.tar.gz',
-
-		'flash-player-11.0.1.152-windows-64bit-uninstaller':
-			'uninstall_flashplayer11_0r1_152_win_64bit.exe',
-
-		'flash-player-11.2.202.223-solaris-sparc-npapi':
-			'flash_player11_2r202_223_solaris_sparc.tar.bz2',
-
-		'flash-player-11.2.202.223-solaris-x86-npapi':
-			'flash_player_11_2r202_223_solaris_x86.tar.bz2',
-
-		'flash-player-11.3.300.262-windows-npapi-debug':
-			'flashplayer11_3r300_262_win_debug.exe',
-
-		'flash-player-11.3.300.262-windows-32bit-sa-debug':
-			'flashplayer11_3r300_262_win_sa_debug_32bit.exe',
-
-		'flash-player-11.5.502.110-windows-npapi-debug':
-			'flashplayer11_5r502_110_win_debug.exe',
-
-		'flash-player-12.0.0.70-windows-uninstaller':
-			'uninstall_flashplayer12_0r0_70_win.exe'
-	};
-
-	for (const entry of children) {
-		if (entry.name in renamedChild) {
-			equal(entry.file, renamedChild[entry.name]);
-		}
-		else {
-			equal(entry.file, basename(entry.source));
 		}
 	}
 }
@@ -192,10 +130,70 @@ async function unique() {
 	}
 }
 
+async function rename() {
+	const {roots, children} = await readPackages();
+
+	const renamedRoot = {
+		// None currently.
+	};
+
+	for (const {name, file, source} of roots) {
+		if (name in renamedRoot) {
+			equal(file, renamedRoot[name]);
+		}
+		else {
+			equal(file, decodeURIComponent(
+				(new URL(source)).pathname.split('/').pop()
+			));
+		}
+	}
+
+	const renamedChild = {
+		'flash-player-9.0.246.0-mac-ub-npapi-debug':
+			'flashplayer9r246_ub_mac_debug.zip',
+
+		'flash-player-10.0.42.34-mac-npapi-debug':
+			'flashplayer10r42_34_ub_mac_debug.zip',
+
+		'flash-player-11.0.1.152-linux-i386-npapi-debug':
+			'flashplayer11_0r1_152_linux_debug.i386.tar.gz',
+
+		'flash-player-11.0.1.152-windows-64bit-uninstaller':
+			'uninstall_flashplayer11_0r1_152_win_64bit.exe',
+
+		'flash-player-11.2.202.223-solaris-sparc-npapi':
+			'flash_player11_2r202_223_solaris_sparc.tar.bz2',
+
+		'flash-player-11.2.202.223-solaris-x86-npapi':
+			'flash_player_11_2r202_223_solaris_x86.tar.bz2',
+
+		'flash-player-11.3.300.262-windows-npapi-debug':
+			'flashplayer11_3r300_262_win_debug.exe',
+
+		'flash-player-11.3.300.262-windows-32bit-sa-debug':
+			'flashplayer11_3r300_262_win_sa_debug_32bit.exe',
+
+		'flash-player-11.5.502.110-windows-npapi-debug':
+			'flashplayer11_5r502_110_win_debug.exe',
+
+		'flash-player-12.0.0.70-windows-uninstaller':
+			'uninstall_flashplayer12_0r0_70_win.exe'
+	};
+
+	for (const {name, file, source} of children) {
+		if (name in renamedChild) {
+			equal(file, renamedChild[name]);
+		}
+		else {
+			equal(file, basename(source));
+		}
+	}
+}
+
 async function main() {
 	await properties();
-	await rename();
 	await unique();
+	await rename();
 }
 main().catch(err => {
 	console.error(err);
