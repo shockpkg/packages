07093f6b121fecdf29d46a783d1653272db16add
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2024-04-21 01:01:10 -0400

    Simpler check

--- bin/newest-harman-air-runtime.mjs
+++ bin/newest-harman-air-runtime.mjs
@@ -2,87 +2,53 @@
 
 /* eslint-disable no-console */
 
-import {readdir} from 'fs/promises';
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
-
-import {read as packageRead} from '../util/package.mjs';
+import {read as readPackages} from '../util/packages.mjs';
 import {buffer as hashBuffer} from '../util/hash.mjs';
 import {userAgent} from '../util/harman-airsdk.mjs';
 import {retry} from '../util/retry.mjs';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const pkgsdir = `${__dirname}/../packages/air-runtime`;
-
-function propSorter(i) {
-	return (a, b) => a[i] - b[i];
-}
-
-async function newest() {
-	const vers = [];
-	for (const f of await readdir(pkgsdir)) {
-		if (f.startsWith('.')) {
-			continue;
-		}
-		const parts = f.split('.yaml');
-		if (parts.length < 2) {
-			continue;
-		}
-		vers.push(parts[0].split('.').map(Number));
-	}
-	vers.sort(propSorter(3));
-	vers.sort(propSorter(2));
-	vers.sort(propSorter(1));
-	vers.sort(propSorter(0));
-	return vers.pop().join('.');
-}
+const files = [
+	'AdobeAIR.exe',
+	'AdobeAIR.dmg'
+];
 
 async function main() {
 	const start = Date.now();
 	const passed = new Set();
 	const failed = new Set();
 
-	const version = await newest();
-	const expected = (await packageRead('air-runtime', version))
-		.map(p => [p.name, p.file, p.size, p.sha256]);
-	for (const [name, file, sized, sha256] of expected) {
-		const source = `https://airsdk.harman.com/assets/downloads/${file}`;
+	const {packages} = await readPackages();
+	const hashed = new Map(packages.map(p => [p.sha256, p]));
 
-		console.log(`Checking: ${name}`);
+	for (const file of files) {
+		console.log(`Checking: ${file}`);
+
+		const source = `https://airsdk.harman.com/assets/downloads/${file}`;
 		console.log(`URL: ${source}`);
 
 		// eslint-disable-next-line no-await-in-loop
 		const response = await retry(() => fetch(source, {
 			'User-Agent': userAgent
 		}));
-		const {status, headers} = response;
+		const {status} = response;
 		if (status !== 200) {
-			failed.add(name);
+			failed.add(file);
 			console.log(`Error: Status code: ${status}: ${source}`);
-			console.log('');
-			continue;
-		}
-
-		const size = +headers.get('content-length');
-		if (size !== sized) {
-			failed.add(name);
-			console.log(`Error: Unexpected size: ${size} != ${sized}`);
 			console.log('');
 			continue;
 		}
 
 		// eslint-disable-next-line no-await-in-loop
 		const body = Buffer.from(await response.arrayBuffer());
-		const [rSha256] = hashBuffer(body, ['sha256']);
-		if (rSha256 !== sha256) {
-			failed.add(name);
-			console.log(`Error: Unexpected sha256: ${rSha256} != ${sha256}`);
+		const [sha256] = hashBuffer(body, ['sha256']);
+		if (!hashed.get(sha256)) {
+			failed.add(file);
+			console.log(`Error: Unknown sha256: ${sha256}`);
 			console.log('');
 			continue;
 		}
 
-		passed.add(name);
+		passed.add(file);
 		console.log('');
 	}
 
