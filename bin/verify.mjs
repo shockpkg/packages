8205ea9744f5666add53526839b114ca156c9c86
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2024-04-29 00:22:19 -0400

    Simpler verify

--- bin/verify.mjs
+++ bin/verify.mjs
@@ -2,8 +2,6 @@
 
 /* eslint-disable no-console */
 
-import {queue as asyncQueue} from 'async';
-
 import packaged from '../util/packages.mjs';
 import {retry} from '../util/util.mjs';
 
@@ -85,153 +83,77 @@ async function getMetadataForUrl(url) {
 }
 
 async function main() {
-	const start = Date.now();
+	// eslint-disable-next-line no-process-env
+	const threads = (+process.env.SHOCKPKG_NEWEST_THREADS) || 4;
+	// eslint-disable-next-line no-process-env
+	const includes = process.env.SHOCKPKG_VERIFY_INCLUDES || '';
+
+	console.log(`Threads: ${threads}`);
 
 	const packages = await packaged();
-	let included = null;
+	let resources = packages;
 
-	// eslint-disable-next-line no-process-env
-	const threads = (+process.env.SHOCKPKG_VERIFY_THREADS) || 4;
-	console.log(`Number of threads: ${threads}`);
-
-	// eslint-disable-next-line no-process-env
-	const retries = (+process.env.SHOCKPKG_VERIFY_RETRY) || 4;
-	console.log(`Number of retries: ${retries}`);
-
-	// eslint-disable-next-line no-process-env
-	const delay = (+process.env.SHOCKPKG_VERIFY_RETRY_DELAY) || 0;
-	console.log(`Delay before retry in milliseconds: ${delay}`);
-
-	// eslint-disable-next-line no-process-env
-	const includes = process.env.SHOCKPKG_VERIFY_INCLUDES || null;
 	if (includes) {
 		const str = JSON.stringify(includes);
-		console.log(`Only checking those names including ${str}`);
-		included = packages.filter(pkg => pkg.name.includes(includes));
-	}
-	else {
-		included = packages;
+		console.log(`Only checking those names including: ${str}`);
+		resources = packages.filter(pkg => pkg.name.includes(includes));
 	}
 
-	console.log(`Checking ${included.length} of ${packages.length}`);
-	console.log('');
+	console.log(`Checking ${resources.length} of ${packages.length}`);
 
-	const retryStat = task => `${task.attempt}/${task.retries}`;
+	const each = async pkg => {
+		const metadata = await getMetadataForUrl(pkg.source);
 
-	const taskReport = (task, err) => {
-		const ms = task.end - task.start;
-		const state = (err ? 'Error' : 'Done');
-		const info = err ? `: ${err.message}` : '';
-		const retry = `[${retryStat(task)}]`;
+		if (metadata.size !== pkg.size) {
+			throw new Error(
+				`Unexpected size: ${metadata.size} (expected ${pkg.size})`
+			);
+		}
 
-		console.log(`${task.pkg.name}: ${state} ${retry} (${ms}ms)${info}`);
+		for (const algo of ['md5', 'sha1', 'sha256']) {
+			if (!metadata[algo]) {
+				continue;
+			}
+			if (metadata[algo] === pkg[algo]) {
+				continue;
+			}
+			throw new Error(
+				`Unexpected ${algo}: ${metadata[algo]} (expected ${pkg[algo]})`
+			);
+		}
+
+		if (metadata.private) {
+			throw new Error('Unexpected private');
+		}
 	};
 
-	const report = await new Promise(resolve => {
-		const report = [];
-		const reporter = (task, err) => {
-			report.push({task, err});
-		};
+	const passed = [];
+	const failed = [];
+	await Promise.all((new Array(threads)).fill(0)
+		.map(async () => {
+			while (resources.length) {
+				const resource = resources.shift();
 
-		const taskEnd = (task, err) => {
-			task.end = Date.now();
-			taskReport(task, err);
-		};
+				console.log(`${resource.name}: ${resource.source}: Checking`);
 
-		const q = asyncQueue(async task => {
-			task.start = Date.now();
-			task.attempt++;
-
-			console.log(`${task.pkg.name}: Checking [${retryStat(task)}]`);
-			const metadata = await getMetadataForUrl(task.pkg.source);
-
-			if (metadata.size !== task.pkg.size) {
-				throw new Error(
-					`Unexpected size: ${metadata.size}` +
-					` (expected ${task.pkg.size})`
-				);
+				// eslint-disable-next-line no-await-in-loop
+				await retry(() => each(resource))
+					.then(() => {
+						console.log(`${resource.name}: Pass`);
+						passed.push(resource);
+					})
+					.catch(err => {
+						console.log(`${resource.name}: Fail: ${err.message}`);
+						failed.push(resource);
+					});
 			}
+		})
+	);
 
-			for (const algo of ['md5', 'sha1', 'sha256']) {
-				if (!metadata[algo]) {
-					continue;
-				}
-				if (metadata[algo] === task.pkg[algo]) {
-					continue;
-				}
-				throw new Error(
-					`Unexpected ${algo}: ${metadata[algo]}` +
-					` (expected ${task.pkg[algo]})`
-				);
-			}
+	console.log(`Passed: ${passed.length}`);
+	console.log(`Failed: ${failed.length}`);
 
-			if (metadata.private) {
-				throw new Error('Unexpected private');
-			}
-
-			taskEnd(task, null);
-			reporter(task, null);
-		}, threads);
-
-		let pending = 0;
-
-		q.error((err, task) => {
-			taskEnd(task, err);
-
-			if (task.attempt < task.retries) {
-				const after = delay ? ` after ${delay}ms` : '';
-				console.log(
-					`${task.pkg.name}: Retrying [${retryStat(task)}]${after}`
-				);
-
-				if (delay) {
-					pending++;
-					setTimeout(() => {
-						q.push(task);
-						pending--;
-					}, delay);
-				}
-				else {
-					q.push(task);
-				}
-			}
-			else {
-				reporter(task, err);
-			}
-		});
-
-		q.drain(() => {
-			if (!pending) {
-				resolve(report);
-			}
-		});
-
-		for (const pkg of included) {
-			q.push({pkg, start: null, end: null, attempt: 0, retries});
-		}
-	});
-	console.log('');
-
-	const reportPassed = report.filter(entry => !entry.err);
-	const reportFailed = report.filter(entry => entry.err);
-
-	if (reportFailed.length) {
-		console.log(`Failed: ${reportFailed.length}`);
-		for (const {task, err} of reportFailed) {
-			if (err) {
-				taskReport(task, err);
-			}
-		}
-		console.log('');
-	}
-
-	const end = Date.now();
-	console.log(`Passed: ${reportPassed.length}`);
-	console.log(`Failed: ${reportFailed.length}`);
-	console.log(`Done after ${end - start}ms`);
-	console.log('');
-
-	if (reportFailed.length) {
+	if (failed.length) {
 		process.exitCode = 1;
 	}
 }
