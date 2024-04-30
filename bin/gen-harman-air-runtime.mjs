0aa7f4525a5a541da99316d0a3dc59e7b026913c
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2024-04-30 00:20:55 -0400

    Smarter gen harman runtime

--- bin/gen-harman-air-runtime.mjs
+++ bin/gen-harman-air-runtime.mjs
@@ -7,32 +7,12 @@ import {stat} from 'node:fs/promises';
 import {ensure} from '../util/gencache.mjs';
 import {file as hashFile} from '../util/hash.mjs';
 import {packages as encodePackages} from '../util/yaml.mjs';
-import {userAgent} from '../util/harman.mjs';
-
-function genList(version, versioned) {
-	const p = versioned ? `${version}/` : '';
-	return [
-		[
-			`air-runtime-${version}-windows`,
-			`https://airsdk.harman.com/assets/downloads/${p}AdobeAIR.exe`
-		],
-		[
-			`air-runtime-${version}-mac`,
-			`https://airsdk.harman.com/assets/downloads/${p}AdobeAIR.dmg`
-		]
-	];
-}
+import {runtimes, userAgent} from '../util/harman.mjs';
 
 async function main() {
-	const args = process.argv.slice(2);
-	if (args.length < 2) {
-		throw new Error('Args: versioned version');
-	}
-	const [versioned, version] = args;
-
 	const doc = [];
-	const list = genList(version, !!+versioned);
-	for (const [name, url] of list) {
+	const resources = await runtimes();
+	for (const {name, file, sha256, source: url} of resources) {
 		console.log(`Name: ${name}`);
 		console.log(`URL: ${url}`);
 
@@ -59,14 +39,19 @@ async function main() {
 		const {size} = await stat(cached.filepath);
 		console.log(`Size: ${size}`);
 
-		const [sha256, sha1, md5] =
-			// eslint-disable-next-line no-await-in-loop
-			await hashFile(cached.filepath, ['sha256', 'sha1', 'md5']);
+		// eslint-disable-next-line no-await-in-loop
+		const [fileSha256, sha1, md5] =	await hashFile(
+			cached.filepath,
+			['sha256', 'sha1', 'md5']
+		);
+		if (fileSha256 !== sha256) {
+			throw new Error(`Unexpected sha256: ${fileSha256} != ${sha256}`);
+		}
+
 		console.log(`SHA256: ${sha256}`);
 		console.log(`SHA1: ${sha1}`);
 		console.log(`MD5: ${md5}`);
 
-		const file = url.split('/').pop();
 		const entry = {
 			name,
 			file,
